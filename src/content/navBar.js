ab674b298c0180f2d787fd623b7c2337b5aea11c
Alfredo Alberruche <aalberrh@gmail.com>
2024-11-24 13:40:28 +0100

    Made scalable

---
+++ src/content/navBar.js
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    { href: "#services", label: "Servicios" },
+    { href: "#working-process", label: "Proceso de trabajo" },
+    { href: "#team", label: "Nuestro equipo" },
+    { href: "#testimonials", label: "Opiniones" },
+  ];
+  
